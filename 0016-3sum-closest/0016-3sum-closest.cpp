da664df61c79eb2a764d55a32ed7a38ee328b575
Taresh Puri <tareshpuri5@gmail.com>
2026-07-19 22:23:45 +0530

    Time: 9 ms (68.9%), Space: 14.1 MB (45.66%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,57 +1,32 @@
-// class Solution {
-// public:
-//     int threeSumClosest(vector<int>& nums, int target) {
-//         int currentSum;
-//           int closestSum = nums[0] + nums[1] + nums[2];    
-        
-
-//         for(int i = 0;i<nums.size() ; i++){
-//          for(int j = i+1;j<nums.size() ; j++){
-//             for (int k=j+1;k<nums.size();k++){
-//              currentSum= nums[i]+nums[j]+nums[k];
-//                  if (abs(currentSum - target) < abs(closestSum - target)) {
-//                         closestSum = currentSum;
-//              }
-//           }
-//         }
-//     } 
-//         return closestSum;
-//     }
-// };
-
-#include <vector>
-#include <algorithm>
-#include <climits>
-using namespace std;
-
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        int n = nums.size();
         sort(nums.begin(), nums.end());
 
-        int closestSum = nums[0] + nums[1] + nums[2]; // initial guess
+        int closest = nums[0] + nums[1] + nums[2];
 
-        for (int i = 0; i < n - 2; i++) {
+        for(int i = 0; i < nums.size()-2; i++){
             int left = i + 1;
-            int right = n - 1;
+            int right = nums.size()-1;
 
-            while (left < right) {
-                int currentSum = nums[i] + nums[left] + nums[right];
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
 
-                if (abs(currentSum - target) < abs(closestSum - target)) {
-                    closestSum = currentSum;
+                if(abs(target-sum) < abs(target-closest)){
+                    closest = sum;
                 }
 
-                if (currentSum < target) {
+                if(sum < target){
                     left++;
-                } else if (currentSum > target) {
+                }
+                else if(sum > target){
                     right--;
-                } else {
-                    return currentSum; // exact match
+                }
+                else{
+                    return sum;
                 }
             }
         }
-        return closestSum;
+        return closest;
     }
 };
