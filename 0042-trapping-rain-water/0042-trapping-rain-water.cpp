c2bf3e0b85f583c4248bf93686c6cd1682ed822d
Taresh Puri <tareshpuri5@gmail.com>
2026-09-20 12:45:42 +0530

    Time: 0 ms (100%), Space: 26 MB (77.62%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int left = 0;
-        int right_max = 0;
-        int left_max = 0;
-        int water = 0;
-       int right = height.size()- 1;
-        while(left < right ){
-            // if (right < right_max){
-            //     right_max = right;
-            //     right --;
-            // }else if (left > left_max){
-            //         left_max = left;
-            //         left ++;
-            //     }
-            // }
-            // if ()
-            // max(height[right],height[left])
-            if (height[left] <= height[right]){
-                // height_left = height[left];
-                if (left_max<height[left]){
-                   left_max = height[left];
-                }else{
-                    water += left_max-height[left];
+       int water =0;
+       int left=0;
+       int right = height.size()-1;
+       int left_max=0;
+       int right_max=0;
+
+
+       while (left<right){
+            if (height[left]<=height[right]){ 
+                 if(left_max<height[left]){
+                    left_max=height[left];
                 }
-                left++;
-            }
-            else{
-                // height_right = height[right];
-                if (right_max<height[right]){
-                   right_max = height[right];
-                }else{
-                    water += right_max-height[right];
+                else{
+                    water += left_max - height[left];
                 }
-                right--;
+                 left++;   
+                
+           
+            }else{
+                if(right_max<height[right]){
+                    right_max=height[right];
+                }
+                else{
+                    water += right_max - height[right];
+                }
+                 right--;
+                
+                
+              
             }
-        }
-        return water;
+       }
+       return water;
     }
 };
